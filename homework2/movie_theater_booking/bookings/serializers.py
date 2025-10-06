213e0de5df24449a24748272a307376ca3c175d3
Logan Andersen <landerse@uccs.edu>
2025-10-06 21:49:02 +0000

    actually added serializers

---
+++ homework2/movie_theater_booking/bookings/serializers.py
@@ -0,0 +1,17 @@
+# Make serializers for the models
+from bookings.models import Movie, Seat, Booking
+
+class MovieSerializer(serializers.HyperlinkedModelSerializer) :
+    class Meta :
+        model = Movie
+        fields = ['title', 'description', 'release_date', 'duration']
+
+class SeatSerializer(serializers.HyperlinkedModelSerializer) :
+    class Meta :
+        model = Seat
+        fields = ['seat_number', 'booking_status']
+
+class BookingSerializer(serializers.HyperlinkedModelSerializer) :
+    class Meta:
+        model = Booking
+        fields = ['movie', 'seat', 'user', 'boooking_date'] 
