7517b202773664d50f4d6f89567a1cf63cd1d4a9
Logan Andersen <landerse@uccs.edu>
2025-10-06 15:33:32 +0000

    moved models.py to right spot (in bookings)

--- homework2/movie_theater_booking/bookings/models.py
+++ homework2/movie_theater_booking/bookings/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+class Movie(models.Model) :
+    title = models.CharField(max_length=200)
+    description = models.CharField(max_length=200)
+    release_date = models.DateTimeField("release date")
+    duration = models.DurationField()
+
+class Seat(models.Model) :
+    # Add choices for booking status
+    # See https://docs.djangoproject.com/en/4.2/ref/models/fields/#choices
+    PENDING = "PND"
+    CONFIRMED = "CON"
+    CANCELED = "CAN"
+    COMPLETED = "COM"
+    NO_SHOW = "NSH"
+    UNSCHEDULED = "UNSCH"
+
+    BOOKING_CHOICES = [ 
+        (PENDING, "Pending"),
+        (CONFIRMED, "Confirmed"),
+        (CANCELED, "Canceled"),
+        (COMPLETED, "Completed"),
+        (NO_SHOW, "Now Show"),
+        (UNSCHEDULED, "Unscheduled")
+    ]
+
+    seat_number = PositiveIntegerField("seat number")
+    booking_status = models.CharField("booking status",max_length=5,choices=BOOKING_CHOICES,default=UNSCHEDULED)
+
+class Movie(models.Model) :
+    movie = models.CharField(max_length=200)
+    seat = models.CharField(max_length=200)
+    user = models.CharField(max_length=200)
+    boooking_date = models.DateTimeField("booking date")
